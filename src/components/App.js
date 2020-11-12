fc2f077a8ee55c5ceb0bfa1c5ac3eaf89532e1a8
Milda-Grabyte <milda.grabyte@gmail.com>
2020-11-12 08:58:34 +0100

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../styles/App.scss';
+
+class App extends React.Component {
+  render() {
+    return (
+    <div>hola mundo</div>
+    );
+  }
+}
+
+export default App;
